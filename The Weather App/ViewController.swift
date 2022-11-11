941cb96b7577bc3241e8d2bcc9795d2f1821d7c7
Qendrim Mjeku <qendrimmjeku@gmail.com>
2022-11-11 16:37:55 +0100

    initial commit

---
+++ The Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Weather App
+//
+//  Created by Qendrim Mjeku on 11.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

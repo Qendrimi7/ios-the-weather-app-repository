267beb6d807b3c67270a806570b3981cd3267029
Qendrim Mjeku <qendrimmjeku@gmail.com>
2022-11-12 17:19:07 +0100

    add some colours

---
+++ The Weather App/Resources/Theme/Theme.swift
@@ -0,0 +1,17 @@
+//
+//  Theme.swift
+//  The Weather App
+//
+//  Created by Qendrim Mjeku on 12.11.22.
+//
+
+import UIKit
+
+class Theme {
+    static let appTintColor = UIColor(named: "AppTintColor")
+    static let launchBackgroundColor = UIColor(named: "LaunchBackgroundColor")
+    static let primaryColor = UIColor(named: "PrimaryColor")
+    static let secondaryColor = UIColor(named: "SecondaryColor")
+    static let titleLabelColor = UIColor(named: "TitleLabelColor")
+    static let descriptionLabelColor = UIColor(named: "DescriptionLabelColor")
+}
